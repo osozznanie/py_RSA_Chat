7e69cd78f2511204ef8d0a8ca10394fa2a0cb9c8
Vladislav <vlad.chertorygin18@gmail.com>
2024-05-28 09:44:34 +0300

    add

---
+++ client1.py
@@ -0,0 +1,90 @@
+import socket
+import threading
+
+import custom_rsa
+from constants import *
+
+
+class Client:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.public_key, self.private_key = custom_rsa.key_generation()
+        self.server_socket = socket.socket()
+
+    def start(self):
+        self.server_socket.connect((self.host, self.port))
+        print(f'Connected to {self.host}:{self.port}')
+
+        # Send own public key to the server
+        self.server_socket.send(str(self.public_key).encode())
+
+        # Receive the other client's public key from the server
+        other_public_key = self.receive_public_key()
+        print(f'Received other client public key: {other_public_key}')
+
+        # Proceed with communication using the received public key
+        self.communicate(other_public_key)
+
+    def receive_public_key(self):
+        other_public_key = self.server_socket.recv(ENCRYPTED_MESSAGE_SIZE).decode()
+        other_public_key = tuple(map(int, other_public_key.strip(ROUND_BRACKETS).split(COMMA)))
+        return other_public_key
+
+    def communicate(self, other_public_key):
+        receive_thread = threading.Thread(target=self.receive_messages)
+        receive_thread.start()
+
+        send_thread = threading.Thread(target=self.send_messages, args=(other_public_key,))
+        send_thread.start()
+
+        receive_thread.join()
+        send_thread.join()
+
+    def send_messages(self, other_public_key):
+        try:
+            while True:
+                print('Enter a message: ', end='', flush=True)
+                message = input()
+                print("\033[1A\033[2K", end='', flush=True)
+                if message.lower() == EXIT_MESSAGE:
+                    break
+                elif not message:
+                    print('Message cannot be empty.')
+                    continue
+
+                encrypted_message = custom_rsa.encrypt(message, other_public_key)
+                self.server_socket.send(str(encrypted_message).encode())
+                print("\033[93m\033[1A\033[2KMessage sent to the other client...\033[0m")
+        except (KeyboardInterrupt, ConnectionResetError):
+            print('Connection closed.')
+        finally:
+            self.server_socket.close()
+
+    def receive_messages(self):
+        while True:
+            try:
+                encrypted_reply = self.server_socket.recv(ENCRYPTED_MESSAGE_SIZE).decode()
+                if not encrypted_reply:
+                    print('Empty reply received.')
+                    continue
+
+                encrypted_reply = list(map(int, encrypted_reply.strip(SQUARE_BRACKETS).split(COMMA)))
+                reply = custom_rsa.decrypt(encrypted_reply, self.private_key)
+
+                # Save the current cursor position
+                print("\033[s", end='')
+
+                # Move the cursor up one line, clear the line, and print the received message
+                print("\033[1A\033[2K\033[94mReceived: {}\033[0m".format(reply))
+
+                # Restore the cursor position and reprint the input prompt
+                print("\033[uEnter a message: ", end='', flush=True)
+            except (KeyboardInterrupt, ConnectionResetError):
+                print('Connection closed.')
+                break
+
+
+if __name__ == '__main__':
+    client = Client(SERVER_HOST, SERVER_PORT)
+    client.start()
--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import socket
-import custom_rsa
+import threading
 from constants import *
 
 
@@ -8,59 +8,59 @@ class Server:
         self.host = host
         self.port = port
         self.limit_listen = limit_listen
-        self.public_key, self.private_key = custom_rsa.key_generation()
-        self.client_public_key = None
-        self.client_socket = None
         self.server_socket = socket.socket()
+        self.client_sockets = []
 
     def start(self):
         self.server_socket.bind((self.host, self.port))
         self.server_socket.listen(self.limit_listen)
         print(f'Server is listening on {self.host}:{self.port}...')
 
-        self.client_socket, client_address = self.server_socket.accept()
-        print(f'Connection from {client_address} has been established.')
+        while len(self.client_sockets) < 2:
+            client_socket, client_address = self.server_socket.accept()
+            self.client_sockets.append(client_socket)
+            print(f"Client {client_address} connected.")
 
-        self.client_socket.send(str(self.public_key).encode())
+        # After two clients are connected, exchange their public keys and start handling them
+        if len(self.client_sockets) == 2:
+            self.exchange_keys()
+            for client_socket in self.client_sockets:
+                client_thread = threading.Thread(target=self.handle_client, args=(client_socket,))
+                client_thread.start()
 
-        self.client_public_key = self.receive_public_key()
-        client_name = CLIENT_NAME
-        print(f'Client name: {client_name}')
-
-        self.communicate(client_name)
-
-    def receive_public_key(self):
-        client_public_key = self.client_socket.recv(ENCRYPTED_MESSAGE_SIZE).decode()
-        client_public_key = tuple(map(int, client_public_key.strip(ROUND_BRACKETS).split(COMMA)))
-        print(f'Received client public key: {client_public_key}')
-        return client_public_key
-
-    def communicate(self, client_name):
-        try:
-            while True:
-                message = input('Enter a message: ')
-                if message.lower() == EXIT_MESSAGE:
-                    break
-                elif not message or message == '':
-                    print('Message cannot be empty.')
+    def handle_client(self, client_socket):
+        other_client_socket = next((cs for cs in self.client_sockets if cs is not client_socket), None)
+        while True:
+            try:
+                # Receive encrypted message from the client
+                encrypted_message = client_socket.recv(ENCRYPTED_MESSAGE_SIZE).decode()
+                print(f"Received message from {client_socket.getpeername()}: {encrypted_message}")
+                if not encrypted_message:
+                    print('Empty message received.')
                     continue
 
-                encrypted_message = custom_rsa.encrypt(message, self.client_public_key)
-                self.client_socket.send(str(encrypted_message).encode())
-                print("\033[93mMessage sent to the client...\033[0m")
+                # Send the encrypted message to the other client
+                other_client_socket.send(encrypted_message.encode())
+            except Exception as e:
+                print(f"An error occurred: {e}")
+                self.client_sockets.remove(client_socket)
+                client_socket.close()
+                break
 
-                encrypted_reply = self.client_socket.recv(ENCRYPTED_MESSAGE_SIZE).decode()
-                if not encrypted_reply:
-                    print('Empty reply received from the client.')
-                    continue
+    def exchange_keys(self):
+        client1_socket, client2_socket = self.client_sockets
 
-                encrypted_reply = list(map(int, encrypted_reply.strip(SQUARE_BRACKETS).split(COMMA)))
-                reply = custom_rsa.decrypt(encrypted_reply, self.private_key)
-                print(f'{client_name}: {reply}')
-        except (KeyboardInterrupt, ConnectionResetError):
-            print('Connection closed.')
-        finally:
-            self.client_socket.close()
+        # Receive public key from client 1
+        client1_public_key = client1_socket.recv(ENCRYPTED_MESSAGE_SIZE).decode()
+        # Receive public key from client 2
+        client2_public_key = client2_socket.recv(ENCRYPTED_MESSAGE_SIZE).decode()
+
+        # Send client 2's public key to client 1
+        client1_socket.send(client2_public_key.encode())
+        # Send client 1's public key to client 2
+        client2_socket.send(client1_public_key.encode())
+
+        print('Public keys exchanged between clients.')
 
 
 if __name__ == '__main__':
